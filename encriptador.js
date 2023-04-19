ae98714215d692b6de88b2668a2d969f52f4407c
Joseph BS <jjbs1805@gmail.com>
2023-04-18 20:10:42 -0500

    use RegEx to encrypt and decrypt text

--- encriptador.js
+++ encriptador.js
@@ -1,8 +1,28 @@
 
-function encriptador (text){
+function encriptador(text) {
+  const chars = {
+    a: "ai",
+    e: "enter",
+    i: "imes",
+    o: "ober",
+    u: "ufat",
+  };
 
+  var s = text;
+  s = s.replace(/[aeiou]/g, (m) => chars[m]);
+  return s;
 }
 
-function desencriptador (text){
-
+function desencriptador(text) {
+    const chars = {
+        ai: "a",
+        enter: "e",
+        imes: "i",
+        ober: "o",
+        ufat: "u",
+      };
+    
+      var s = text;
+      s = s.replace(/(?:ai|enter|imes|ober|ufat)/g, m=> chars[m]);
+    return s;
 }

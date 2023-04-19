50e0ea73283694860686b6ff8237cdc668f89f69
Joseph BS <jjbs1805@gmail.com>
2023-04-19 14:43:05 -0500

    finish main content without copy buttom

--- encriptador.js
+++ encriptador.js
@@ -1,6 +1,24 @@
-function getTextAreaContent() {
+function getTextAreaContentEncrypted() {
+    var noContenDivExist = true
     var text = document.getElementById("input-txtarea");
     var textValue = text.value;
+
+    const divOutPut = document.getElementById("output-nocontent")
+
+
+    //REFACTOR
+    if (divOutPut == null) {
+        noContenDivExist = false
+        console.log("No existe el img nocontent")
+    } else {
+        var parent = divOutPut.parentNode;
+        console.log("existe el img nocontent")
+    }
+
+    var div = document.createElement('div');
+
+    var containerDiv = document.querySelector(".output-area")
+    containerDiv.appendChild(div)
 
     if (textValue == "") {
         Swal.fire({
@@ -9,10 +27,77 @@ function getTextAreaContent() {
             confirmButtonColor: '#0a3871',
         });
     } else {
+        if (noContenDivExist) parent.removeChild(divOutPut);
+        var encryptedText = encriptador(textValue)
         
-        alert(encriptador(textValue));
 
+        if(htmlElementExist("output-txtarea")){
+            var outputTex = document.getElementById("output-txtarea");
+            outputTex.value= encryptedText;
+        }else{
+            showOutputTxtArea(div, encryptedText);
+        }
+        
+        
     }
+}
+
+function getTextAreaContentDecrypted() {
+    var noContenDivExist = true
+    var text = document.getElementById("input-txtarea");
+    var textValue = text.value;
+
+    const divOutPut = document.getElementById("output-nocontent")
+
+
+    //REFACTOR
+    if (divOutPut == null) {
+        noContenDivExist = false
+        console.log("No existe el img nocontent")
+    } else {
+        var parent = divOutPut.parentNode;
+        console.log("existe el img nocontent")
+    }
+
+    var div = document.createElement('div');
+
+    var containerDiv = document.querySelector(".output-area")
+    containerDiv.appendChild(div)
+
+    if (textValue == "") {
+        Swal.fire({
+            title: "Ingrese un mensaje ☺",
+            icon: "warning",
+            confirmButtonColor: '#0a3871',
+        });
+    } else {
+        if (noContenDivExist) parent.removeChild(divOutPut);
+        var encryptedText = desencriptador(textValue)
+        
+
+        if(htmlElementExist("output-txtarea")){
+            var outputTex = document.getElementById("output-txtarea");
+            outputTex.value= encryptedText;
+        }else{
+            showOutputTxtArea(div, encryptedText);
+        }
+        
+        
+    }
+
+}
+
+function showOutputTxtArea(div, text) {
+    let html = `<textarea disabled name="" id="output-txtarea" cols="20" rows="10"  class="output-txtarea">${text}</textarea> 
+                    <input type="submit" value="Copiar" class="bt-copiar" />`;
+
+    div.insertAdjacentHTML('beforeend', html);
+}
+
+function htmlElementExist(idElement) {
+    var divOutPut = document.getElementById(idElement)
+    if (divOutPut != null) return true;
+    else return false;
 }
 
 function encriptador(text) {
